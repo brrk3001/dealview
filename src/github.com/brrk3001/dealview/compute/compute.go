58a9cc69eaf2249ae94e8b4648296cb8b76bc2dd
RK Bettadapura <rk@RKs-MacBook-Pro-2.local>
2021-02-21 10:20:49 +0530

    What

--- src/github.com/brrk3001/dealview/compute/compute.go
+++ src/github.com/brrk3001/dealview/compute/compute.go
@@ -20,13 +20,15 @@ func CompletionPercentage(DateStart, DateEnd, RevYearStart, RevYearEnd string) f
 	if error1 != nil || error2 != nil || error3 != nil || error4 != nil {
 
 		fmt.Println(error1, error2, error3, error4)
+		panic(fmt.Sprintf("Exiting"))
 	}
 
 	var RecognizedDaysInYear time.Duration = 0
 	DaysInYr, error := time.ParseDuration("8760h") //no leap years for now, 8760h in a year
 
 	if error != nil {
-		return 0
+		fmt.Println(error)
+		panic(fmt.Sprintf("Exiting"))
 	}
 	DealDuration := end.Sub(start)
 	fmt.Println(end, start, DealDuration)
@@ -39,32 +41,42 @@ func CompletionPercentage(DateStart, DateEnd, RevYearStart, RevYearEnd string) f
 	DaysFromRyStartToDealStart := start.Sub(rystart)
 	DaysFromRyEndToDealEnd := end.Sub(ryend)
 	DaysFromRyEndToDealStart := start.Sub(ryend)
-	//fmt.Println(DaysFromRyStartToDealEnd, DaysFromRyStartToDealStart, DaysFromRyEndToDealEnd,DaysFromRyEndToDealStart)
+
+	fmt.Println(DaysFromRyStartToDealEnd, DaysFromRyStartToDealStart, DaysFromRyEndToDealEnd, DaysFromRyEndToDealStart)
 
 	if DaysFromRyStartToDealEnd < 0 || DaysFromRyEndToDealStart > 0 {
 		return 0.0
 	}
 
-	if DaysFromRyEndToDealEnd < 0 && DaysFromRyStartToDealStart > 0 {
+	if DaysFromRyEndToDealEnd <= 0 && DaysFromRyStartToDealStart >= 0 { //starts, ends completely within year
 		return 1.0
 	}
 
-	if DaysFromRyStartToDealStart > 0 { //deal ends after RY end but starts within RY
-		RecognizedDaysInYear = DaysInYr - DaysFromRyStartToDealStart
-	} else { //deal starts before RYstart but ends within RY
+	if DaysFromRyEndToDealEnd > 0 { //deal ends after RY end but starts within or before RY
+
+		if DaysFromRyStartToDealStart > 0 { //Starts after RY but ends after RYend
+			RecognizedDaysInYear = DaysInYr - DaysFromRyStartToDealStart
+		} else {
+			RecognizedDaysInYear = DaysInYr
+		}
+	} else { //deal starts before RYstart but ends within  or at RY
 		RecognizedDaysInYear = DaysFromRyStartToDealEnd
 	}
 
-	return float64(RecognizedDaysInYear / DaysInYr)
+	return float64(RecognizedDaysInYear.Hours() / DealDuration.Hours())
+}
+
+func TestCompletionPercentage() {
+
+	end := "08/02/2007"
+	start := "04/02/2006"
+	rystart := "04/01/2006"
+	ryend := "03/31/2007"
+	fmt.Println(CompletionPercentage(start, end, rystart, ryend))
 
 }
 
 func main() {
-
-	v2 := "03/01/2007"
-	v1 := "02/02/2007"
-	v3 := "04/01/2006"
-	v4 := "03/31/2007"
-	fmt.Println(CompletionPercentage(v1, v2, v3, v4))
+	TestCompletionPercentage()
 
 }
