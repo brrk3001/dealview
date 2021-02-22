fd2b26b568dd26a48f625172abbc4257b7653fae
RK Bettadapura <rk@RKs-MacBook-Pro-2.local>
2021-02-22 22:55:59 +0530

    Adding sheets api and basic deal structure

---
+++ src/github.com/brrk3001/dealview/talk/api/crm.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Deal struct { //stores all mandatory fields
+	dealNum     int
+	title       int
+	company     int
+	owner       string
+	value       float64
+	subCategory string
+	dateStart   string
+	dateEnd     string
+}
+
+func main() {
+	fmt.Println("vim-go")
+}
